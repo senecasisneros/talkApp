4ecdd05e7557b662ebfa85898ff2bd10121564a9
Seneca Sisneros <seneca.sisneros@yahoo.com>
2017-10-13 19:06:59 -0700

    added CreateComment.js and index.js component. Added CreateComment component to Comments container

---
+++ src/components/CreateComment.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+
+class CreateComment extends Component {
+  render() {
+    return (
+      <div>
+        Create Comment
+      </div>
+    )
+  }
+}
+
+export default CreateComment;
---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import CreateComment from './CreateComment';
+import Comment from './Comment';
+import Location from './Location';
+
+export {
+  CreateComment,
+  Comment,
+  Location
+}
